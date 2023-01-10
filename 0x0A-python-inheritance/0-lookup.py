45dd463850959c2e13749fce1a21579e6493292f
Oni Michael <onisamuelmichael@gmail.com>
2023-01-10 19:28:35 +0100

    Delete 0x0A-python-inheritance directory
    done

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Define the lookup function"""
-
-
-def lookup(obj):
-    """Return the list of attributes of an object"""
-    return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Define MyList class"""
-
-class MyList(list):
-	"""Representation of MyList class"""
-	
-	def print_sorted(self):
-		"""print list in sorted order"""
-		print(sorted(self))
